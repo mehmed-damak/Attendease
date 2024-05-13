a52342cbbebb9ac699a367b710573b7cc6b53973
Mehmed <65054270+mehmed-damak@users.noreply.github.com>
2024-05-14 00:36:20 +0100

    Add the scanning capability to scan in students in realtime

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from website import create_app
+from website import create_app, models
+from website import db
+from website.models import *
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = create_app()
 
+scheduler=BackgroundScheduler
+scheduler.init_app(app)
+def backgroundtask():
+    reader = SimpleMFRC522()
+    id, text = reader.read()
+    courses=Course.query.all()
+    students=User.query.all()
+    activecourse=Course.query.filter_by(status=True)
+    currentstudent=User.query.filter_by(firstName=text)
+    currentrelation=User.query.filter_by(course_id=activecourse.id, user_id=currentstudent.id)
+    currentrelation.attendance = currentrelation.attendance + 1       
+    
+scheduler.add_job(backgroundtask, "interval", seconds=2)
+
 if __name__ == '__main__':
+    scheduler.start()
     app.run(host='0.0.0.0', port=5000, debug=True)
     #This runs the app
-    
-#code for rfid scanner
